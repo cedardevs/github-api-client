627c33738d1c5667ac4ef74d6a88e0cab22a8141
dneufeld <david.neufeld@colorado.edu>
2019-05-10 16:13:44 -0600

    HTML formatting added.

--- src/githubIssuesAPI.groovy
+++ src/githubIssuesAPI.groovy
@@ -20,12 +20,12 @@ def page = 1
 def queryParams = ""
 
 def nextPage = true
-def epicFilter = false
 
-File file = new File("${config.repo}Issues.txt")
+File file = new File("${config.repo}Issues.html")
 def writer = file.newWriter()
 def totCnt = 0
-
+def storyList = []
+def epicList = []
 
 while ( nextPage ) {
     def storyCnt = 0
@@ -37,11 +37,15 @@ while ( nextPage ) {
     connection.setRequestProperty( 'User-Agent', 'groovy-2.4.x' )
     connection.setRequestProperty( 'Accept', 'application/json' )
     //BasicAuth
-    String encoded = Base64.getEncoder().encodeToString((config.username+":"+config.password).getBytes(StandardCharsets.UTF_8));  //Java 8
-    connection.setRequestProperty("Authorization", "Basic "+encoded);
+    String encoded = Base64.getEncoder().encodeToString((config.username+":"+config.password).getBytes(StandardCharsets.UTF_8))  //Java 8
+    connection.setRequestProperty("Authorization", "Basic "+encoded)
 
-    writer << "Github issue report for ${config.repo}\n"
-    writer << "Closed issues since ${config.dateSince}\n\n"
+    writer << "<!DOCTYPE HTML>\n"
+    writer << "<html>\n"
+    writer << "<head><title>Github Report</title></head>\n"
+    writer << "<body>\n"
+    writer << "<p><b>Issue report for the ${config.repo} repo</b></p>\n"
+    writer << "<p><b><i>Work completed since ${config.dateSince}</i></b></p>\n\n"
 
 
     if ( connection.responseCode == 200 ) {
@@ -60,37 +64,44 @@ while ( nextPage ) {
 
             issueText = new StringBuffer("")
 
-            issueText << "Title: ${issue.title}\n"
-            issueText << "  Status: ${issue.state}\n"
-            issueText << "  Status: ${issue.url}\n"
-            issueText << "  Description: ${issue.body}\n"
-            issueText << "  Create: ${issue.created_at}\n"
-            issueText << "  Labels: ${issue.labels.name}\n"
-            issueText << "  Closed: ${issue.closed_at}\n\n"
+            issueText << "<li>${issue.title.capitalize()} (${config.repo} <a href='${issue.html_url}' target='_blank'>#${issue.number}</a>)</li>\n"
+            //issueText << "  Status: ${issue.state}<br>\n"
+            //issueText << "  Status: ${issue.url}<br>\n"
+            //issueText << "  Description: ${issue.body}<br>\n"
+            //issueText << "  Create: ${issue.created_at}<br>\n"
+            //issueText << "  Labels: ${issue.labels.name}<br>\n"
+            //issueText << "  Closed: ${issue.closed_at}<br><br>\n\n"
 
-            if ( !epicFilter ) {
 
-                // write out all issues
-                writer << issueText
+            // write out just epics
+            if ("${issue.title}".contains("EPIC")) {
+                epicList.add( issueText )
 
             } else {
-
-                // write out just epics
-                if ("${issue.title}".contains("EPIC")) {
-                    writer << issueText
-                    epicCnt++
-                }
-
+                storyList.add( issueText )
             }
-            storyCnt++
+
             totCnt++
-            writer.flush()
         }
-        
-        writer << "\n\n"
-        if ( epicFilter ) writer << "Epic Count: ${epicCnt}\n"
-        //writer << "Story Count: ${storyCnt}\n"
-        //writer << "Page Count: ${page}\n"
+        writer << "<u>Epics:</u><br>\n"
+        writer << "<ul>"
+        epicList.each {
+            writer << "${it}"
+        }
+        writer << "</ul>"
+
+        writer << "<br>\n"
+
+        writer << "<u>Stories:</u><br>\n"
+        writer << "<ul>\n"
+        storyList.each {
+            writer << "${it}"
+        }
+        writer << "</ul>\n"
+        writer.flush()
+
+
+        writer << "<br>\n"
         
     } else {
 
@@ -106,7 +117,10 @@ while ( nextPage ) {
     if ( storyCnt < 100 || page > 20) nextPage = false
     
 }
-writer << "Total Issues: ${totCnt}\n"
+writer << "<b>Total Issues: ${totCnt}</b><br>\n"
+
+writer << "</body>\n"
+writer << "</html>\n"
 writer.close()
 
 println "Total Issues: ${totCnt}\n"

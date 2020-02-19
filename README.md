829bc569504835c612a23f353bcb3b7fdfc469e4
ken.tanaka <ken.tanaka@noaa.gov>
2020-02-19 12:35:35 -0700

    Enhancements to use include the issue body in the ouput with an alternate formatting option

--- README.md
+++ README.md
@@ -3,14 +3,26 @@
 Groovy code to download issues from the github api.
 
 Code assumes a config.yaml file exists with credentials needed to access the repository.
-Note: Two-factor authentication should be off to avoid a 401 Unauthorized HTTP error.
 
 Results written as an HTML report.
 
-## Example:
+## Example config.yaml:
  - username: "myname"
  - password: "mypassword"
  - repo: "myrepo"
  - dateSince: "YYYY-MM-DD"
+ - includeBody: false
+ - useHeaders: false
 
-  
+Note: If two-factor authentication is on, you will encounter a 401 Unauthorized HTTP error with your normal login password.
+
+Use a Personal Access Token in place of your normal password, so you can leave your interactive login protected by two-factor 
+authentication. To generate the personalAccessToken view the instructions at
+
+https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
+
+**includeBody** can enable printing the body of the issue (markdown converted to HTML). This can be handy for presenting
+information to people who do not have permission to view the linked issue numbers. 
+
+**useHeaders** turns on an alternate formatting option using headers for the section text instead of underlined text. 
+This helps the Epic, Closed and Open Stories section text stand out better when issue bodies are included.
--- src/githubIssuesAPI.groovy
+++ src/githubIssuesAPI.groovy
@@ -1,19 +1,46 @@
+import org.apache.tools.bzip2.CBZip2OutputStream
 
 @Grab(group='org.yaml', module='snakeyaml', version='1.8')
+@Grab(group = 'com.atlassian.commonmark', module = 'commonmark', version = '0.14.0')
 
 import java.time.LocalDate
 import groovy.json.JsonSlurper
 import java.nio.charset.StandardCharsets
 import org.yaml.snakeyaml.Yaml
+import org.commonmark.node.*
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
 
 Yaml parser = new Yaml()
 def config = parser.load(("config.yaml" as File).text)
 
-println config.username
-//println config.password
-println config.repo
-println config.dateSince
+println "username: ${config.username}"
+//println "password: ${config.password}"
+println "repo: ${config.repo}"
+println "dateSince: ${config.dateSince}"
+println "includeBody: ${config.includeBody}"
+println "useHeaders: ${config.useHeaders}"
+println '-' * 10
 
+Parser mdParser // Markdown parser
+Node docuemnt
+HtmlRenderer renderer
+if (config.includeBody) {
+    mdParser = Parser.builder().build()
+    renderer = HtmlRenderer.builder().build()
+}
+
+// Section text
+String startOfEpics = "<u>Epics:</u><br>\n"
+String startOfClosedStories = "<u>Closed Stories:</u><br>\n"
+String startOfOpenStories = "<u>Open Stories:</u><br>\n"
+Boolean boldIssueTitles = false
+if (config.useHeaders) {
+    startOfEpics = "<h2>Epics:</h2>\n"
+    startOfClosedStories = "<h2>Closed Stories:</h2>\n"
+    startOfOpenStories = "<h2>Open Stories:</h2>\n"
+    boldIssueTitles = true
+}
 
 def page = 1
 
@@ -31,7 +58,7 @@ writer << "<html>\n"
 writer << "<head><title>Github Report</title></head>\n"
 writer << "<body>\n"
 writer << "<p><h1>Sprint Recap for ${config.repo}</h1></p>\n"
-writer << "<p><h3>Dates: ${config.dateSince} to ${today}</h3></p>\n\n"
+writer << "<p><b>Dates: ${config.dateSince} to ${today}</b></p>\n\n"
 
 def openList = []
 def closedList = []
@@ -50,6 +77,12 @@ while ( nextPage ) {
     String encoded = Base64.getEncoder().encodeToString((config.username+":"+config.password).getBytes(StandardCharsets.UTF_8))  //Java 8
     connection.setRequestProperty("Authorization", "Basic "+encoded)
 
+    // Personal Access Token, acts as OAuth for development usage
+//    String tokenParam = "${config.personalAccessToken}:x-oauth-basic"
+    //String authString = "Basic" + Base64.getEncoder().encodeToString(tokenParam.getBytes(StandardCharsets.UTF_8))  //Java 8
+//    String authString = "Basic" + Base64.getEncoder().encodeToString(tokenParam.getBytes())  //Java 8
+//    connection.setRequestProperty("Authorization", authString)
+
 
     if ( connection.responseCode == 200 ) {
         
@@ -66,7 +99,19 @@ while ( nextPage ) {
         json.each { issue ->
             storyCnt++
             issueText = new StringBuffer("")
-            issueText << "<li>${issue.title.capitalize()} (${config.repo} <a href='${issue.html_url}' target='_blank'>#${issue.number}</a>)</li>\n"
+            issueText << "<li>"
+            if (boldIssueTitles) {
+                issueText << "<b>${issue.title.capitalize()}</b>"
+            }
+            else {
+                issueText << "${issue.title.capitalize()}"
+            }
+            issueText << " (${config.repo} <a href='${issue.html_url}' target='_blank'>#${issue.number}</a>)</li>\n"
+            if (config.includeBody && issue.body) {
+                document = mdParser.parse(issue.body)
+                String issueHtml = renderer.render(document)
+                issueText << "<dd><table style='width: 100%; border: 1px solid black; background-color: #eee;'><tr><td>${issueHtml}</td></tr></table></dd>\n"
+            }
 
             // write out just epics
             if ("${issue.title}".contains("EPIC")) {
@@ -102,7 +147,7 @@ while ( nextPage ) {
 
     
 }
-writer << "<u>Epics:</u><br>\n"
+writer << startOfEpics
 writer << "<ul>"
 epicList.each {
     writer << "${it}"
@@ -111,7 +156,7 @@ writer << "</ul>"
 
 writer << "<br>\n"
 
-writer << "<u>Closed Stories:</u><br>\n"
+writer << startOfClosedStories
 writer << "<ul>\n"
 closedList.each {
     writer << "${it}"
@@ -120,7 +165,7 @@ writer << "</ul>\n"
 
 writer << "<br>\n"
 
-writer << "<u>Open Stories:</u><br>\n"
+writer << startOfOpenStories
 writer << "<ul>\n"
 openList.each {
     writer << "${it}"
